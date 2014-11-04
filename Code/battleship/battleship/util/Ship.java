af7b1466fd4ed3713efe9efcc3794465c4393fdb
Fran Lozano <franloza@ucm.es>
2014-11-04 11:17:01 +0100

    Update Ship.java

--- Code/battleship/battleship/util/Ship.java
+++ Code/battleship/battleship/util/Ship.java
@@ -1,26 +1,6 @@
 package battleship.util;
 
-import battleship.util.Ship;
+public class Ship {
 
-/** 
- *   
- *    Class to hold the ships while being received from UI
- */
-public class Army {
-
-  private Ship[] ships;
-
-  	public Army(int n) {
-  		this.ships = new Ship[n];
-  	}
-  
-	public Ship[] getShips() {
-		return ships;
-	}
-	
-	public void setShipInPosition(int n, Vector to, Vector from)
-	{
-		this.ships[n] = new Ship(to, from);
-	}
-  
 }
+
